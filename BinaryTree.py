a06c38e150ef5a22a39e89d089931cf0894d7637
Kristine <chankristineagcay@gmmail.com>
2023-01-14 13:56:08 +0800

    Added add-child Function

--- BinaryTree.py
+++ BinaryTree.py
@@ -3,3 +3,19 @@ class BinarySearchTreeNode:
         self.data = data
         self.left = None
         self.right = None
+
+# Data Checking        
+def add_child(self, data):
+        if data == self.data:
+            return # node already exists
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
