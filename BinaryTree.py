21430e92c57b9fae333f0fe441ed6961ed48538a
Kristine <chankristineagcay@gmmail.com>
2023-01-14 15:18:09 +0800

    Implement Maximum and Minimum Function

--- BinaryTree.py
+++ BinaryTree.py
@@ -42,6 +42,31 @@ class BinarySearchTreeNode:
             elements += self.right.pre_order_traversal()
 
         return elements
+  
+# Implement Post Order Traversal Function  
+    def post_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.post_order_traversal()
+        if self.right:
+            elements += self.right.post_order_traversal()
+
+        elements.append(self.data)
+
+        return elements
+ 
+# Finding Maximum  
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        return self.right.find_max()
+    
+# Finding Minimum
+    def find_min(self):
+        if self.left is None:
+            return self.data
+        return self.left.find_min()
+
 
 # Implement Build Tree Function
 def build_tree(elements):
@@ -62,7 +87,12 @@ if __name__ == '__main__':
     print('╚═*.·:·.✧ ✦ ✧.·:·.**.·:·.✧ ✦ ✧.·:·.**.·:·.✧ ✦ ✧.·:·.**.·:·.✧ ✦ ✧.·:·.**.·:·.✧ ✦ ✧.·:·.**.·:·.✧ ✦ ✧.·:·.*═╝')
 
     print()
-    print("\033[01m[1]\33[0m \033[92mIn Order Traversal sorted list:\33[0m",letters_tree.in_order_traversal())
-    print("\033[01m[2]\33[0m \033[92mPre-Order Traversal sorted list:\33[0m",letters_tree.pre_order_traversal())
+    print("\033[01m[1]\33[0m \033[92mMaximum:\33[0m",letters_tree.find_max())
+    print("\033[01m[2]\33[0m \033[92mMinimum:\33[0m",letters_tree.find_min())
+    print("\033[01m[3]\33[0m \033[92mIn Order Traversal sorted list:\33[0m",letters_tree.in_order_traversal())
+    print("\033[01m[4]\33[0m \033[92mPre-Order Traversal sorted list:\33[0m",letters_tree.pre_order_traversal())
+    print("\033[01m[5]\33[0m \033[92mPost Order Traversal sorted list:\33[0m",letters_tree.post_order_traversal())
+
+    
 
     
